b8c9bfb715b82b157127f78bd08bf565673ce6fc
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-05-03 12:01:11 +0900

    vpc/subnet

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -29,3 +29,48 @@ resource "aws_internet_gateway" "main_igw" {
 }
 #############################################################################
 
+
+############################# Public Subnet #################################
+resource "aws_subnet" "webserver" {
+    count = length(var.cidrs["public"])
+
+    vpc_id = aws_vpc.main_vpc.id
+    cidr_block = var.cidrs["public"][count.index]
+    availability_zone = local.az_names[count.index]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = join("", ["webserver_", local.subnet_purpose[count.index]])
+    }
+}
+#############################################################################
+
+############################# Private Subnet #################################
+resource "aws_subnet" "was" {
+    count = length(var.cidrs["private"])
+
+    vpc_id = aws_vpc.main_vpc.id
+    cidr_block = var.cidrs["private"][count.index]
+    availability_zone = local.az_names[count.index]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = join("", ["was_", local.subnet_purpose[count.index]])
+    }
+}
+#############################################################################
+
+############################# Database Subnet #################################
+resource "aws_subnet" "database" {
+    count = length(var.cidrs["database"])
+
+    vpc_id = aws_vpc.main_vpc.id
+    cidr_block = var.cidrs["database"][count.index]
+    availability_zone = local.az_names[count.index]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = join("", ["database_", local.subnet_purpose[count.index]])
+    }
+}
+#############################################################################
