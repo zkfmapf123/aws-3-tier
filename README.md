06c8e33970c39dfc46da4b54072c2565368bfbb3
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-05-28 22:19:35 +0900

    [ADD] vpn

--- README.md
+++ README.md
@@ -14,8 +14,25 @@
 
 ![3-ter](./public/3-tier.png)
 
+## OpenVPN 설정
+
+- OpenVPN을 사용하여 Private Subnet으로 접근한다
+
+![vpn](./public/vpn.png)
+
+```
+  ssh -i id_rsa openvpnas@public_ip
+  https://public_ip:943/admin
+
+  id : openvpn
+  pw : ~~~~~~
+
+  > user Management (host, Guest 지정)
+```
+
 ## Caution
 
 - ap-northeast-2의 경우 t3로 진행해야 함 (t2는 불가)
 - t3.nano는 ssh provisioning이 안됨... 네트워크 불량? + 저사양? => t3.micro 이상으로 해야함
 - EC2 에서 MarketPlace AMI는 구독을 해야 생성이 되네?
+- openVPN EC2 접속할 때는 > ssh -i "" openvpnas@public_ip
--- infra/modules/security-group/openVPN/main.tf
+++ infra/modules/security-group/openVPN/main.tf
@@ -15,28 +15,28 @@ resource "aws_security_group" "openVPN_sg" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = [var.vpc_info.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port   = 943
         to_port     = 943
         protocol    = "tcp"
-        cidr_blocks = [var.vpc_info.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port   = 443
         to_port     = 443
         protocol    = "tcp"
-        cidr_blocks = [var.vpc_info.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port   = 1194
         to_port     = 1194
         protocol    = "udp"
-        cidr_blocks = [var.vpc_info.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
